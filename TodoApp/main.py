51581b52a0483dfb284894210d5a209b943d1e77
SouvickMazumdar <69057478+SouvickMazumdar@users.noreply.github.com>
2026-05-31 13:47:54 +0530

    Added the home rediredt

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -7,7 +7,7 @@ It includes database connection management, dependency injection, and a simple G
 
 
 # Import FastAPI and Depends for API creation and dependency injection
-from fastapi import FastAPI,Request  # FastAPI framework and dependency injection
+from fastapi import FastAPI,Request,status  # FastAPI framework and dependency injection
 
 # # Import models to access Base for metadata
 from .models import Base  # To access Base for table creation
@@ -15,26 +15,32 @@ from .models import Base  # To access Base for table creation
 from .database import engine # DB engine and session factory
 
 from .routers import auth, todos, admin, users
-from fastapi.templating import Jinja2Templates
+# from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 # Create FastAPI app instance
 app = FastAPI()  # Initialize FastAPI app
 # Create all tables in the database (if not already created)
 Base.metadata.create_all(bind=engine)  # Create tables from models
 
-templates=Jinja2Templates(directory= "TodoApp/templates")
+# templates=Jinja2Templates(directory= "TodoApp/templates")
 
 
 app.mount("/static",StaticFiles(directory="TodoApp/static"), name="static")
 
 
 
+# @app.get("/")
+# def test(request: Request):
+#     # This is called template rendering <{"request":request}> this context can be used inside the webpage, when this webpage is rendered
+#     # throough endpoint you can pass only the json but through template rendering you can pass the values
+#     return templates.TemplateResponse("home.html",{"request":request})
+
 @app.get("/")
 def test(request: Request):
-    # This is called template rendering <{"request":request}> this context can be used inside the webpage, when this webpage is rendered
-    # throough endpoint you can pass only the json but through template rendering you can pass the values
-    return templates.TemplateResponse("home.html",{"request":request})
+    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
+
 @app.get("/healthy")
 def health_check():
     return {'status': 'Healthy'}
